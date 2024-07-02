3e3d4e46447431ee6a0fb57735149210fbc578b7
Kyle <teerzo@live.com.au>
2024-07-02 20:37:37 +1000

    Fix popup content

--- popup.js
+++ popup.js
@@ -1,2 +1,9 @@
 
 // console.log('popup');
+
+const domPopupLink = document.getElementById('popupLink');
+if( domPopupLink ) {
+    console.log('check', chrome.runtime.id);
+    const url = `chrome-extension://${chrome.runtime.id}/index.html`
+    domPopupLink.href = url;
+}
